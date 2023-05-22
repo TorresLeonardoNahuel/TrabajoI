bc549a77d9e851d9f2cfd558822ee4ae5e8f72a2
TorresLeonardoNahuel <torres.leonardo.nahuel@gmail.com>
2023-05-22 16:50:47 -0300

    Se modifico contenido de todas las paginas y estilo

--- common.js
+++ common.js
@@ -4,16 +4,42 @@ $(function() {
     // Cargar el pie de página
     $("#footer").load("footer.html");
   });
+
+//   Para el Boton de Editar y Guardar en Mi Cuenta
+  function habilitarEdicion() {
+    var inputs = document.getElementsByTagName("input");
+    for (var i = 0; i < inputs.length; i++) {
+      if (inputs[i].type !== "button") {
+        inputs[i].removeAttribute("disabled");
+      }
+    }
+
+    document.querySelector('input[value="Editar"]').style.display = "none";
+    document.querySelector('input[value="Guardar"]').style.display = "inline-block";
+  }
+
+  function deshabilitarEdicion() {
+    var inputs = document.getElementsByTagName("input");
+    for (var i = 0; i < inputs.length; i++) {
+      if (inputs[i].type !== "button") {
+        inputs[i].setAttribute("disabled", "disabled");
+      }
+    }
+
+    document.querySelector('input[value="Editar"]').style.display = "inline-block";
+    document.querySelector('input[value="Guardar"]').style.display = "none";
+  
+  }
   
   
-  //Para productos agregar y quitar producto al carrito
+  //Para productos agregar al carrito
   function agregarAlCarrito(nombre, precio) {
     var cart = document.querySelector('.cart');
     var item = document.createElement('div');
     item.classList.add('item');
     item.innerHTML = '<span class="name">' + nombre + '</span><span class="price">$ ' + precio.toFixed(2) + '</span>';
     var removeButton = document.createElement('button');
-    removeButton.classList.add('remove');
+    removeButton.classList.add('btn');
     removeButton.textContent = 'Eliminar';
     removeButton.addEventListener('click', function() {
         quitarDelCarrito(item, precio);
@@ -26,7 +52,7 @@ $(function() {
     total += precio;
     totalElement.innerHTML = 'Total: $ ' + total.toFixed(2);
     }
-
+//Para productos quitar producto al carrito
 function quitarDelCarrito(item, precio) {
     var cart = document.querySelector('.cart');
     cart.removeChild(item);
